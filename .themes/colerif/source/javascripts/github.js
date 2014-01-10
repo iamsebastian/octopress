d602a17373f9ee3c8d37907c1285528419f532f4
Sebastian Blei <s.blei@plazz-entertainment.com>
2014-01-10 09:05:47 +0100

    github plugin rewritten

--- .themes/colerif/source/javascripts/github.js
+++ .themes/colerif/source/javascripts/github.js
@@ -1,32 +1,32 @@
 var github = (function(){
-  function escapeHtml(str) {
-    return $('<div/>').text(str).html();
-  }
+  // function escapeHtml(str) {
+    // return str;
+    // return document.querySelector('<div/>').innerHTML;
+  // }
   function render(target, repos){
-    var i = 0, fragment = '', t = $(target)[0];
+    var i = 0, fragment = '', t = document.querySelector(target);
 
+    console.log('repos',repos);
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
+      fragment += '<li><span class="githubrepo"><a href="'+repos[i].html_url+'">'+repos[i].name+'</a></span><p>'+repos[i].description||''+'</p></li>';
     }
     t.innerHTML = fragment;
   }
   return {
     showRepos: function(options){
-      $.ajax({
-          url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed&callback=?"
-        , dataType: 'jsonp'
-        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
-        , success: function(data) {
-          var repos = [];
-          if (!data || !data.data) { return; }
-          for (var i = 0; i < data.data.length; i++) {
-            if (options.skip_forks && data.data[i].fork) { continue; }
-            repos.push(data.data[i]);
-          }
-          if (options.count) { repos.splice(options.count); }
-          render(options.target, repos);
+      var x = new XMLHttpRequest();
+      x.onload = function() {
+        var r = JSON.parse(this.response);
+        var repos = [];
+        for (var i = 0; i < r.length; i++) {
+          if (options.skip_forks && r[i].fork) { continue; }
+          repos.push(r[i]);
         }
-      });
+        if (options.count) { repos.splice(options.count); }
+        render(options.target, repos);
+      };
+      x.open('GET', 'https://api.github.com/users/iamsebastian/repos?sort=pushed', true);
+      x.send();
     }
   };
 })();
