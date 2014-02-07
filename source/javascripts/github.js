1adc7b56eba33372e2ffaa4373016e3859020364
Sebastian Blei <s.blei@plazz-entertainment.com>
2014-02-07 16:04:25 +0100

    sassy

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -1,14 +1,14 @@
+'use strict';
 var github = (function(){
-  function escapeHtml(str) {
-    return str;
+  // function escapeHtml(str) {
+    // return str;
     // return document.querySelector('<div/>').innerHTML;
-  }
+  // }
   function render(target, repos){
     var i = 0, fragment = '', t = document.querySelector(target);
-
     console.log('repos',repos);
     for(i = 0; i < repos.length; i++) {
-      fragment += '<li><span class="githubrepo"><a href="'+repos[i].html_url+'">'+repos[i].name+'</a></span><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
+      fragment += '<li><span class="githubrepo"><a href="'+repos[i].html_url+'">'+repos[i].name+'</a></span><p>'+repos[i].description||''+'</p></li>';
     }
     t.innerHTML = fragment;
   }
@@ -27,21 +27,6 @@ var github = (function(){
       };
       x.open('GET', 'https://api.github.com/users/iamsebastian/repos?sort=pushed', true);
       x.send();
-      // $.ajax({
-      //     url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed&callback=?"
-      //   , dataType: 'jsonp'
-      //   , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
-      //   , success: function(data) {
-      //     var repos = [];
-      //     if (!data || !data.data) { return; }
-      //     for (var i = 0; i < data.data.length; i++) {
-      //       if (options.skip_forks && data.data[i].fork) { continue; }
-      //       repos.push(data.data[i]);
-      //     }
-      //     if (options.count) { repos.splice(options.count); }
-      //     render(options.target, repos);
-      //   }
-      // });
     }
   };
 })();
