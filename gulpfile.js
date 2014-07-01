947b2bd56137828206cb7258922a6eb1c0d08050
Sebastian Blei <s.blei@plazz-entertainment.com>
2014-07-01 15:56:18 +0200

    Advanced gulp configuration - inbetween Rake tasks.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+/* global require */
+
+var dir = 'public',
+  paths = {
+    dest: dir,
+    css: dir + '/stylesheets/screen.css',
+    html: dir + '/index.html'
+};
+
+var gulp = require('gulp'),
+    minifyHTML = require('gulp-minify-html'),
+    inlineCss = require('gulp-inline-css');
+
+gulp.task('minify-html', function() {
+  var opts = {
+    comments: false,
+    spare: false
+  };
+
+  gulp.src(paths.html)
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest(paths.dest))
+});
+
+gulp.task('inline-css', function() {
+  return gulp.src(paths.html)
+    .pipe(inlineCss({
+      applyStyleTags: true,
+      applyLinkTags: true,
+      removeStyleTags: true,
+      removeLinkTags: true
+    }))
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('default', ['minify-html'], function() {
+  
+});
